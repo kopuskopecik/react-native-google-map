9e2c58f850be1e44c174e46e509946895b5193f2
erdo <kopuskopecik@gmail.com>
2020-12-25 14:18:14 +0200

    Create city item

--- src/Main.js
+++ src/Main.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import {SafeAreaView, View} from 'react-native';
+import Axios from 'axios';
+import React, {useEffect, useState} from 'react';
+import {SafeAreaView, View, Text, FlatList} from 'react-native';
 import MapView from 'react-native-maps';
 import {City, RestaurantDetail, SearchBar} from './components';
 
 const Main = (props) => {
+  const [cityList, setCityList] = useState([]);
+
+  const fetchCities = async () => {
+    const {data} = await Axios.get(
+      'https://opentable.herokuapp.com/api/cities',
+    );
+    setCityList(data.cities);
+  };
+
+  useEffect(() => {
+    fetchCities();
+  }, []);
+
   return (
     <SafeAreaView style={{flex: 1}}>
       <View style={{flex: 1}}>
         <MapView
           style={{flex: 1}}
           initialRegion={{
-            latitude: 200.78825,
+            latitude: 37.78825,
             longitude: -122.4324,
             latitudeDelta: 0.0922,
             longitudeDelta: 0.0421,
           }}
         />
-          <View style={{position: 'absolute'}}>
-
+        <View style={{position: 'absolute'}}>
           <SearchBar />
-
-          </View>
+          <FlatList
+            horizontal
+            keyExtractor={(_, index) => index.toString()}
+            data={cityList}
+            renderItem={({item}) => <City cityName={item} />}
+          />
+        </View>
       </View>
     </SafeAreaView>
   );
